a8c392db01d5a94306643c38c400831fc0b5ce2f
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-11-21 22:28:54 +1100

    update .gitignore

--- draft-code/fashion-mnist-test.py
+++ draft-code/fashion-mnist-test.py
@@ -1,6 +1,66 @@
 import pennylane as qml
-import pennylane.numpy as np
+import pennylane.numpy as pnp
+import numpy as np
+from typing import List, Tuple, Union
 
+def add_padding(matrix: np.ndarray,
+                padding: Tuple[int, int]) -> np.ndarray:
+    """Adds padding to the matrix.
+    from https://github.com/detkov/Convolution-From-Scratch/blob/main/convolution.py
+    Args:
+        matrix (np.ndarray): Matrix that needs to be padded. Type is List[List[float]] casted to np.ndarray.
+        padding (Tuple[int, int]): Tuple with number of rows and columns to be padded. With the `(r, c)` padding we
+        addding `r` rows to the top and bottom and `c` columns to the left and to the right of the matrix
+    Returns:
+        np.ndarray: Padded matrix with shape `n + 2 * r, m + 2 * c`.
+    """
+    n, m = matrix.shape
+    r, c = padding
+
+    padded_matrix = np.zeros((n + r * 2, m + c * 2))
+    padded_matrix[r: n + r, c: m + c] = matrix
+
+    return padded_matrix
+
+def _check_params(matrix, kernel, stride, dilation, padding):
+    """
+    from https://github.com/detkov/Convolution-From-Scratch/blob/main/convolution.py
+    :param matrix:
+    :param kernel:
+    :param stride:
+    :param dilation:
+    :param padding:
+    :return:
+    """
+    params_are_correct = (isinstance(stride[0], int) and isinstance(stride[1], int) and
+                          isinstance(dilation[0], int) and isinstance(dilation[1], int) and
+                          isinstance(padding[0], int) and isinstance(padding[1], int) and
+                          stride[0] >= 1 and stride[1] >= 1 and
+                          dilation[0] >= 1 and dilation[1] >= 1 and
+                          padding[0] >= 0 and padding[1] >= 0)
+    assert params_are_correct, 'Parameters should be integers equal or greater than default values.'
+    if not isinstance(matrix, np.ndarray):
+        matrix = np.array(matrix)
+    n, m = matrix.shape
+    matrix = matrix if list(padding) == [0, 0] else add_padding(matrix, padding)
+    n_p, m_p = matrix.shape
+
+    if not isinstance(kernel, np.ndarray):
+        kernel = np.array(kernel)
+    k = kernel.shape
+
+    kernel_is_correct = k[0] % 2 == 1 and k[1] % 2 == 1
+    assert kernel_is_correct, 'Kernel shape should be odd.'
+    matrix_to_kernel_is_correct = n_p >= k[0] and m_p >= k[1]
+    assert matrix_to_kernel_is_correct, 'Kernel can\'t be bigger than matrix in terms of shape.'
+
+    h_out = np.floor((n + 2 * padding[0] - k[0] - (k[0] - 1) * (dilation[0] - 1)) / stride[0]).astype(int) + 1
+    w_out = np.floor((m + 2 * padding[1] - k[1] - (k[1] - 1) * (dilation[1] - 1)) / stride[1]).astype(int) + 1
+    out_dimensions_are_correct = h_out > 0 and w_out > 0
+    assert out_dimensions_are_correct, 'Can\'t apply input parameters, one of resulting output dimension is ' \
+                                       'non-positive.'
+
+    return matrix, kernel, k, h_out, w_out
 
 
 
@@ -28,12 +88,15 @@ def load_mnist(path, kind='train'):
 
     return images, labels
 
+
+
 if __name__ == '__main__':
     from PIL import Image
     data_folder = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/fashion"
     im, labels = load_mnist(data_folder)
-    test_index = 1234
+    test_index = 123
     im_test = Image.fromarray(im[test_index].reshape(28,28))
+    print(len(im))
     print(im_test)
     im_test_label = labels[test_index]
     im_test.save(f"test-image-label-{im_test_label}.pdf")
