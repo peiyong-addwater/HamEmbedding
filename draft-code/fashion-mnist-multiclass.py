3a74c8963c9ab65cc38d0ee1d0fa8f1798de3089
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-11-24 15:23:09 +1100

    updates

--- draft-code/fashion-mnist-multiclass.py
+++ draft-code/fashion-mnist-multiclass.py
@@ -242,8 +242,8 @@ if __name__ == '__main__':
     FINAL_LAYER_QUBITS = 3
 
     n_test = 100
-    n_epochs = 2000
-    n_reps = 2
+    n_epochs = 500
+    n_reps = 8
 
     _, _, _, num_wires,_ = _check_params(np.random.rand(28*28).reshape(28,28), kernel=np.random.random(KERNEL_SIZE), stride=STRIDE, dilation=(1,1), padding=(0,0))
     print(num_wires)
