a27c3cd15d29dea0c72515c74da4c4495a683d8a
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-11-28 17:37:07 +1100

    updates

--- draft-code/fashion-mnist-single-qubit-convolution/fashion-mnist-multiclass.py
+++ draft-code/fashion-mnist-single-qubit-convolution/fashion-mnist-multiclass.py
@@ -237,7 +237,7 @@ if __name__ == '__main__':
     rng = np.random.default_rng(seed=seed)
 
     KERNEL_SIZE = (3,3)
-    STRIDE = (5,5)
+    STRIDE = (3,3)
     NUM_CONV_POOL_LAYERS = 2
     FINAL_LAYER_QUBITS = 2
     NUM_CLASSES = 4
