a8741d54680f79ec219b370fd5e64f13cb6634ae
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-12-31 03:14:45 +1100

    updates

--- draft-code/try-dask-jobqueue/test-dask-jobqueue.sh
+++ draft-code/try-dask-jobqueue/test-dask-jobqueue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 #SBATCH --job-name=testDaskJobQueue
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=03:30:00
 #SBATCH --account=pawsey0419
 #SBATCH --export=NONE
 Export OMP_NUM_THREADS=24
