e36a828c9655ffb6de949929810500d1d8cc038f
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-01-02 12:36:09 +1100

    updates

--- draft-code/qiskit-draft-code/fashion-mnist-single-qubit-conv/fashion-mnist-qiskit.sh
+++ draft-code/qiskit-draft-code/fashion-mnist-single-qubit-conv/fashion-mnist-qiskit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 #SBATCH --job-name=qiskitFMNIST
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=00:06:00
 #SBATCH --account=pawsey0419
 #SBATCH --export=NONE
 #SBATCH --mem=16G
