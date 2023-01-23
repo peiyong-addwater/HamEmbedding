bb1d11a1c3bdbcfbaf54281a3802c199fe57d9eb
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-01-23 15:52:08 +1100

    updates

--- draft-code/qiskit-draft-code/new-conv-prototype/conv-5x5-stride-5-28x28.py
+++ draft-code/qiskit-draft-code/new-conv-prototype/conv-5x5-stride-5-28x28.py
@@ -618,7 +618,7 @@ if __name__ == '__main__':
                     f"{round(avg_epoch_time, 4)}, total time {round(time_till_now, 4)}")
 
         bounds = [(0, 2 * np.pi)] * (15*4+45+18+ 15+3*8+15+3*4*2 + 15)
-        # COBYLA single iteration around 97 seconds, SPSA (noisyopt) is 150 seconds at the same condition.
+
         opt = SPSA(maxiter=n_epochs, callback=callback_fn_qiskit_spsa)
         # opt = COBYLA(maxiter=n_epochs, callback=callback_fn)
         res = opt.minimize(
