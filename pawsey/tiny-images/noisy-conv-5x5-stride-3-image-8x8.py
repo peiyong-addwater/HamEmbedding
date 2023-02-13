3ee32ce791f62da08a07d41303a9b0321411c230
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-02-13 18:26:27 +1100

    updates

--- pawsey/tiny-images/noisy-conv-5x5-stride-3-image-8x8.py
+++ pawsey/tiny-images/noisy-conv-5x5-stride-3-image-8x8.py
@@ -7,6 +7,7 @@ from qiskit import Aer
 from qiskit_aer import AerSimulator
 from concurrent.futures import ThreadPoolExecutor
 from qiskit.algorithms.optimizers import SPSA
+from qiskit_aer.noise import NoiseModel
 import json
 import time
 import shutup
@@ -16,4 +17,10 @@ shutup.please()
 
 WKDIR = "/scratch/pawsey0419/peiyongw/QML-ImageClassification/pawsey/tiny-images"
 DATA_PATH = "/scratch/pawsey0419/peiyongw/QML-ImageClassification/data/mini-digits/tiny-handwritten.pkl"
+NOISE_MODEL_PATH = "/scratch/pawsey0419/peiyongw/QML-ImageClassification/pawsey/tiny-images/ibm_perth-20230213-161111.noise_model"
+
+with open(NOISE_MODEL_PATH, 'rb') as f:
+    ibm_perth_noise_model = pickle.load(f)
+
+print(ibm_perth_noise_model)
 
