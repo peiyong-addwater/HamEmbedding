4ad24adb88153ce53e98cfe35d237cd64530d075
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-02-14 14:06:55 +1100

    updates

--- pawsey/tiny-images/noisy8x8.sh
+++ pawsey/tiny-images/noisy8x8.sh
@@ -4,7 +4,9 @@
 #SBATCH --time=3:00:00
 #SBATCH --account=pawsey0419
 #SBATCH --export=NONE
-#SBATCH --mem=64G
+#SBATCH --mem=128G
+
+export OMP_NUM_THREADS=2
 
 module load hpc-python-collection/2022.11-py3.9.15
 
