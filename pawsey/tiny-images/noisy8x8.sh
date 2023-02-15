ac79e0d937c725160b70a6869d4a58bff963bb48
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-02-15 13:13:45 +1100

    updates

--- pawsey/tiny-images/noisy8x8.sh
+++ pawsey/tiny-images/noisy8x8.sh
@@ -7,11 +7,13 @@
 #SBATCH --export=NONE
 #SBATCH --mem=512G
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK   #To define the number of threads, in this case will be 32
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PLACES=cores     #To bind threads to cores
 export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as possible). This option works together with the "places" indicated above, then: allocates threads in closest cores.
 
 
 module load hpc-python-collection/2022.11-py3.9.15
 
-python noisy-conv-5x5-stride-3-image-8x8.py
+echo $OMP_NUM_THREADS
+
+srun -N 1 -n 1 python noisy-conv-5x5-stride-3-image-8x8.py
