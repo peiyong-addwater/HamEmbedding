7d8c794a6170641d14fe33b4a0527042cc4da834
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-02-20 15:13:18 +1100

    updates

--- pawsey/tiny-images/noisy8x8.sh
+++ pawsey/tiny-images/noisy8x8.sh
@@ -1,11 +1,12 @@
 #!/bin/bash --login
 #SBATCH --job-name=noisy8x8
-#SBATCH --partition=work
-#SBATCH --nodes=4
+#SBATCH --partition=hugemem
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --time=24:00:00
 #SBATCH --account=pawsey0419
 #SBATCH --export=NONE
-#SBATCH --mem=800G
+#SBATCH --mem=512G
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PLACES=cores     #To bind threads to cores
@@ -16,4 +17,4 @@ module load hpc-python-collection/2022.11-py3.9.15
 
 echo $OMP_NUM_THREADS
 
-srun -N 2 -n 1 python noisy-conv-5x5-stride-3-image-8x8.py
+srun -N 1 -n 1 python noisy-conv-5x5-stride-3-image-8x8.py
