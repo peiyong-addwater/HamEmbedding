58928aa65d28e3e12b01bfa566e8eeeb3333ddb5
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-05-20 16:31:01 +1000

    updates

--- draft-code/qiskit-draft-code/new-conv-prototype/super-tiny-images/siamese-network/kernel-3x3/kernel-3x3.py
+++ draft-code/qiskit-draft-code/new-conv-prototype/super-tiny-images/siamese-network/kernel-3x3/kernel-3x3.py
@@ -413,15 +413,15 @@ if __name__ == '__main__':
     BACKEND_SIM.set_options(max_job_size=MAX_JOB_SIZE)
     BACKEND_SIM.set_options(max_parallel_experiments=0)
     # BACKEND_SIM.set_options(device='GPU') # GPU is probably more suitable for a few very large circuits instead of a large number of small-to-medium sized circuits
-    seed = 42
+    seed = 1701
     rng = np.random.default_rng(seed=seed)
     KERNEL_SIZE = (3, 3)
     STRIDE = (1, 1)
     n_epochs = 500*2
-    n_img_per_label = 3
+    n_img_per_label = 4
     curr_t = nowtime()
     save_filename = curr_t + "_" + f"siamese-10-class-qiskit-mnist-3x3-conv-classical-features-tiny-image-results-{n_img_per_label}-img_per_class-ADAM-SPSA.json"
-    checkpointfile = "20230515-010140_siamese-10-class-qiskit-mnist-3x3-conv-classical-features-tiny-image-results-2-img_per_class-ADAM-SPSA.json"
+    checkpointfile = "20230516-153220_siamese-10-class-qiskit-mnist-3x3-conv-classical-features-tiny-image-results-3-img_per_class-ADAM-SPSA.json"
     if checkpointfile is not None:
         with open(checkpointfile, 'r') as f:
             checkpoint = json.load(f)
