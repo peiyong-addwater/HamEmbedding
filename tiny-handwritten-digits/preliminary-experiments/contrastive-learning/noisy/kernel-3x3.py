dc2b45a0ca2c641d4e8df841adf53278e5ff2f75
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-06 12:30:50 +1000

    re-org files

--- tiny-handwritten-digits/preliminary-experiments/contrastive-learning/noisy/kernel-3x3.py
+++ tiny-handwritten-digits/preliminary-experiments/contrastive-learning/noisy/kernel-3x3.py
@@ -425,7 +425,7 @@ if __name__ == '__main__':
     KERNEL_SIZE = (3, 3)
     STRIDE = (1, 1)
     n_epochs = 1000
-    n_img_per_label = 2
+    n_img_per_label = 3
     curr_t = nowtime()
     save_filename = curr_t + "_" + f"siamese-10-class-qiskit-mnist-3x3-conv-classical-features-tiny-image-results-{n_img_per_label}-img_per_class-ADAM-SPSA-NOISY.json"
     checkpointfile = "20230606-001107_siamese-10-class-qiskit-mnist-3x3-conv-classical-features-tiny-image-results-2-img_per_class-ADAM-SPSA-NOISY.json"
