27df2b9b69dec5ff7f4be0072fde5101918d434e
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-20 03:23:08 +1000

    updates

---
+++ tiny-handwritten-digits/contrastive_learning/with_qiskit/backbone_circ_with_hierarchical_encoding.py
@@ -0,0 +1,20 @@
+import math
+import numpy as np
+from typing import List, Tuple, Union
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
+from qiskit import Aer
+from concurrent.futures import ThreadPoolExecutor
+from qiskit.algorithms.optimizers import SPSA, COBYLA
+import json
+import time
+import shutup
+import pickle
+import sys
+sys.path.insert(0, '/home/peiyongw/Desktop/Research/QML-ImageClassification')
+
+from SPSAGradOptimiser.qiskit_opts.SPSA_Adam import ADAMSPSA
+from qiskit.circuit import ParameterVector
+import os
+
+from two_by_two_patch_D_and_R import FourPixelDepositAndReverse
+
