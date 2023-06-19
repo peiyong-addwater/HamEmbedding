b132611942ec0022478d81d3dd9ebf81030a252b
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-20 03:13:08 +1000

    Add Deposit and Reverse for encoding image patch into the phase of a qubit

--- tiny-handwritten-digits/contrastive_learning/with_qiskit/two_by_two_patch_D_and_R.py
+++ tiny-handwritten-digits/contrastive_learning/with_qiskit/two_by_two_patch_D_and_R.py
@@ -19,3 +19,39 @@ from SPSAGradOptimiser.qiskit_opts.SPSA_Adam import ADAMSPSA
 from qiskit.circuit import ParameterVector
 import os
 
+from two_by_two_patch_encode import FourPixelEncodeTwoQubits
+
+def FourPixelDepositAndReverse(
+        pixels:Union[list, np.ndarray, ParameterVector],
+        encode_parameters:Union[List, np.ndarray, ParameterVector],
+        phase_parameters:Union[List, np.ndarray, ParameterVector],
+        to_gate = True
+):
+    """
+
+    :param pixels: four-element array
+    :param encode_parameters: 6N-element array, one-dim, where N is the number of "layers" of data-reuploading
+    :param phase_parameters: L-element array, one-dim, where L is the number of D&R repetitions
+    :param to_gate:
+    :return: a gate or circuit, in which the first qubit will have the image patch encoded as phase information
+    """
+    circ = QuantumCircuit(3, name = "FourPixelDepositAndReverse")
+    n_layers = len(phase_parameters)
+    img_patch_encode_gate = FourPixelEncodeTwoQubits(pixels, encode_parameters, to_gate=to_gate)
+    img_patch_encode_gate_inv = img_patch_encode_gate.inverse()
+    circ.h(0)
+    for i in range(n_layers):
+        circ.append(img_patch_encode_gate, [1, 2])
+        circ.cp(phase_parameters[i], 0, 1)
+        circ.append(img_patch_encode_gate_inv, [1, 2])
+
+    return circ.to_gate(label="FourPixelDepositAndReverse") if to_gate else circ
+
+if __name__ == '__main__':
+
+    x = ParameterVector('x', 4)
+    theta = ParameterVector('θ', 12)
+    phi = ParameterVector('φ', 2)
+    circ = FourPixelDepositAndReverse(x, theta, phi, to_gate=False)
+    circ.draw(output='mpl', style='bw', filename='FourPixelDepositAndReverse.png')
+
