9883407e42862da516d49ccf3b59649c0875bed9
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-21 02:20:32 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_qiskit/backbone_circ_with_hierarchical_encoding.py
+++ tiny-handwritten-digits/contrastive_learning/with_qiskit/backbone_circ_with_hierarchical_encoding.py
@@ -79,7 +79,7 @@ def LocalTokenMixing(
     :param img_patches:  4 image patches, each of which is a 4-element array (ParameterVector) reshaped into a 2 by 2 list, or a 2 by 2 by 4 ndarray
     :param single_patch_encoding_parameter: "θ", 6NL-element array, one-dim, where N is the number of "layers" of data-reuploading and L is the number of D&R repetitions in the single patch D&R
     :param single_patch_d_and_r_phase_parameter: "φ", L-element array, one-dim, where L is the number of D&R repetitions in the single patch D&R
-    :param four_q_param_layer_parameter: "γ", 12N-element array, one-dim, where N is the number of layers of the 4-q parameterised layer
+    :param four_q_param_layer_parameter: "γ", 12K-element array, one-dim, where K is the number of layers of the 4-q parameterised layer
     :param local_token_mixing_phase_parameter: "ω", a one-element array for the phase parameter of the token mixing D&R
     :param to_gate:
     :return:
@@ -98,11 +98,60 @@ def LocalTokenMixing(
 
     return circ.to_gate(label="LocalTokenMixing") if to_gate else circ
 
+def backbone_circ_four_q_feature(
+        image_patches:Union[List[List[ParameterVector]], np.ndarray],
+        single_patch_encoding_parameter:Union[ParameterVector, np.ndarray],
+        single_patch_d_and_r_phase_parameter:Union[ParameterVector, np.ndarray],
+        four_q_param_layer_parameter:Union[ParameterVector, np.ndarray],
+        local_token_mixing_phase_parameter:Union[ParameterVector, np.ndarray, float],
+        finishing_layer_parameter:Union[ParameterVector, np.ndarray]
+):
+    """
+    The backbone circuit for producing the four-qubit feature/representation of an 8 x 8 image.
+    The first 4 qubits contain the feature, and the last 6 qubits are ancilla qubits, which, in an ideal simulation, should be reversed back to the |0> state.
+    :param image_patches: All 16 image patches, each of which is a 4-element array (ParameterVector) reshaped into a 4 by 4 list, or a 4 by 4 by 4 ndarray, in which the last dimension is the original pixels.
+    For an image:
+    [[ 0  1  2  3  4  5  6  7]
+    [ 8  9 10 11 12 13 14 15]
+    [16 17 18 19 20 21 22 23]
+    [24 25 26 27 28 29 30 31]
+    [32 33 34 35 36 37 38 39]
+    [40 41 42 43 44 45 46 47]
+    [48 49 50 51 52 53 54 55]
+    [56 57 58 59 60 61 62 63]]
+    the patches (4 by 4 by 4 array):
+    [[[ 0.  1.  8.  9.]
+    [ 2.  3. 10. 11.]
+    [ 4.  5. 12. 13.]
+    [ 6.  7. 14. 15.]]
+
+    [[16. 17. 24. 25.]
+    [18. 19. 26. 27.]
+    [20. 21. 28. 29.]
+    [22. 23. 30. 31.]]
+
+    [[32. 33. 40. 41.]
+    [34. 35. 42. 43.]
+    [36. 37. 44. 45.]
+    [38. 39. 46. 47.]]
+
+    [[48. 49. 56. 57.]
+    [50. 51. 58. 59.]
+    [52. 53. 60. 61.]
+    [54. 55. 62. 63.]]]
+    :param single_patch_encoding_parameter: "θ", 6NL-element array, one-dim, where N is the number of "layers" of data-reuploading and L is the number of D&R repetitions in the single patch D&R
+    :param single_patch_d_and_r_phase_parameter: "φ", L-element array, one-dim, where L is the number of D&R repetitions in the single patch D&R
+    :param four_q_param_layer_parameter: "γ", 12N-element array, one-dim, where N is the number of layers of the 4-q parameterised layer
+    :param local_token_mixing_phase_parameter: "ω", a one-element array for the phase parameter of the token mixing D&R
+    :param finishing_layer_parameter: "η", 12K-element array, one-dim, where K is the number of layers of the finishing layer
+    :return:
+    """
+
 
 if __name__ == '__main__':
     def cut_8x8_to_2x2(img: np.ndarray):
         # img: 8x8 image
-        # return: 4x4x4 array, each element in the first 4x4 is a flattend patch
+        # return: 4x4x4 array, each element in the first 4x4 is a flattened patch
         patches = np.zeros((4, 4, 4))
         for i in range(4):
             for j in range(4):
