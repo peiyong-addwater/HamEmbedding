ea4ab50a11352c84d53be22f8e24d0f1fde7b8b5
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-26 01:50:07 +1000

    add function for Clifford shadows

--- tiny-handwritten-digits/contrastive_learning/with_qiskit/shadow-state-tomography.py
+++ tiny-handwritten-digits/contrastive_learning/with_qiskit/shadow-state-tomography.py
@@ -5,7 +5,7 @@ from typing import List, Tuple, Union
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
 from qiskit import Aer
 from concurrent.futures import ThreadPoolExecutor
-from qiskit.algorithms.optimizers import SPSA, COBYLA
+import qiskit
 import json
 import time
 import shutup
@@ -33,4 +33,87 @@ s_to_pauli = {
     "Y": pauli_list[2],
     "Z": pauli_list[3],
 }
+
+def bitGateMap(qc, g, qi):
+    '''Map X/Y/Z string to qiskit ops'''
+    if g == "X":
+        qc.h(qi)
+    elif g == "Y":
+        qc.sdg(qi)
+        qc.h(qi)
+    elif g == "Z":
+        pass
+    else:
+        raise NotImplementedError(f"Unknown gate {g}")
+
+def Minv(N, X):
+    '''inverse shadow channel'''
+    return ((2 ** N + 1.)) * X - np.eye(2 ** N)
+
+def cliffordShadow(n_shadows:int,
+                   n_qubits:int,
+                   base_circuit:QuantumCircuit,
+                   shadow_register:Union[QuantumRegister, List[QuantumRegister], List[int]],
+                   device_backend=Aer.get_backend('aer_simulator'),
+                   reps:int=1,
+                   seed = 1701,
+                   parallel:bool=True,
+                   simulation:bool=True
+                   ):
+    """
+    Shadow state tomography with Clifford circuits
+    :param n_shadows: number of classical shadows
+    :param n_qubits: number of qubits in the target state/classical shadow
+    :param base_circuit: the circuit producing the target state
+    :param shadow_register: the registers involving the target state
+    :param device_backend: backend to run the circuit
+    :param reps: number of measurement repetitions/shots
+    :param seed: random seed for generating the Clifford circuit
+    :param simulation: whether running on a simulator
+    :param parallel: whether running in parallel
+    :return:
+    """
+    rng = np.random.default_rng(seed)
+    cliffords = [qiskit.quantum_info.random_clifford(n_qubits, seed=rng) for _ in range(n_shadows)]
+    shadow_circs = []
+    cliffords_dict = {}
+    cliffords_counts_dict = {}
+    if simulation and parallel:
+        N_WORKERS = 11
+        MAX_JOB_SIZE = 1
+        EXC = ThreadPoolExecutor(max_workers=N_WORKERS)
+        device_backend.set_options(executor=EXC)
+        device_backend.set_options(max_job_size=MAX_JOB_SIZE)
+        device_backend.set_options(max_parallel_experiments=0)
+    for i in range(len(cliffords)):
+        shadow_meas = ClassicalRegister(n_qubits, name="shadow")
+        clifford = cliffords[i]
+        qc = base_circuit.copy()
+        qc.add_register(shadow_meas)
+        qc.append(clifford.to_instruction(), shadow_register)
+        qc.measure(shadow_register, shadow_meas)
+        qc.name = f"Shadow_{i}"
+        cliffords_dict[qc.name] = clifford
+        qc = transpile(qc, device_backend)
+        shadow_circs.append(qc)
+    job = device_backend.run(shadow_circs, shots=reps)
+    result_dict = job.result().to_dict()["results"]
+    result_counts = job.result().get_counts()
+    shadows = []
+    for i in range(n_shadows):
+        name = result_dict[i]["header"]["name"]
+        counts = result_counts[i]
+        mat = cliffords_counts_dict[name].adjoint().to_matrix()
+        for bit, count in counts.items():
+            Ub = mat[:, int(bit, 2)] # this is Udag|b>
+            shadows.append(Minv(n_qubits, np.outer(Ub, Ub.conj())) * count)
+    rho_shadow = np.sum(shadows, axis=0) / (n_shadows * reps)
+    return rho_shadow
+
+
+
+
+
+
+
 
