da10e79457b79a1430be8e7915f4e35be73e67f0
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-06-26 21:37:40 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_qiskit/shadow-state-tomography.py
+++ tiny-handwritten-digits/contrastive_learning/with_qiskit/shadow-state-tomography.py
@@ -52,7 +52,7 @@ s_to_pauli = {
     "Z": pauli_list[3],
 }
 
-def bitGateMap(qc, g, qi):
+def bitGateMap(qc:QuantumCircuit, g:str, qi:int):
     '''Map X/Y/Z string to qiskit ops'''
     if g == "X":
         qc.h(qi)
@@ -67,6 +67,18 @@ def bitGateMap(qc, g, qi):
 def Minv(N, X):
     '''inverse shadow channel'''
     return ((2 ** N + 1.)) * X - np.eye(2 ** N)
+
+# Note: Qiskit uses little-endian bit ordering
+def rotGate(g):
+    '''produces gate U such that U|psi> is in Pauli basis g'''
+    if g=="X":
+        return 1/np.sqrt(2)*np.array([[1.,1.],[1.,-1.]])
+    elif g=="Y":
+        return 1/np.sqrt(2)*np.array([[1.,-1.0j],[1.,1.j]])
+    elif g=="Z":
+        return np.eye(2)
+    else:
+        raise NotImplementedError(f"Unknown gate {g}")
 
 def operator_2_norm(R):
     """
@@ -89,7 +101,7 @@ def constructCliffordShadowSingleCirc(n_qubits:int,
                    circ_name:str,
                    shadow_register:Union[QuantumRegister, List[QuantumRegister], List[int]],
                    device_backend=Aer.get_backend('aer_simulator'),
-                   transpile:bool=True):
+                   transpile_circ:bool=True):
     """
 
     :param n_qubits:
@@ -98,7 +110,7 @@ def constructCliffordShadowSingleCirc(n_qubits:int,
     :param circ_name:
     :param shadow_register:
     :param device_backend:
-    :param transpile:
+    :param transpile_circ:
     :return:
     """
     #TODO: parallel construction of clifford shadow circuits
@@ -145,7 +157,7 @@ def cliffordShadow(n_shadows:int,
 
 
     for i in range(len(cliffords)):
-        print(i)
+        #print(i)
         shadow_meas = ClassicalRegister(n_qubits, name="shadow")
         clifford = cliffords[i]
         qc = base_circuit.copy()
@@ -157,6 +169,7 @@ def cliffordShadow(n_shadows:int,
         cliffords_dict[f"Shadow_{i}"] = clifford
         shadow_circs.append(qc)
     name_list = [qc.name for qc in shadow_circs]
+    #TODO: parallel transpilation of the circuits
     job = device_backend.run(shadow_circs, shots=reps)
     result_dict = job.result().to_dict()["results"]
     result_counts = job.result().get_counts()
@@ -168,6 +181,100 @@ def cliffordShadow(n_shadows:int,
         for bit, count in counts.items():
             Ub = mat[:, int(bit, 2)] # this is Udag|b>
             shadows.append(Minv(n_qubits, np.outer(Ub, Ub.conj())) * count)
+    rho_shadow = np.sum(shadows, axis=0) / (n_shadows * reps)
+    return rho_shadow
+
+def constructPauliShadowSingleCirc(n_qubits:int,
+                                   base_circuit:QuantumCircuit,
+                                   pauli_string:List[str],
+                                   circ_name:str,
+                                   shadow_register:Union[QuantumRegister, List[QuantumRegister], List[int]],
+                                   device_backend=Aer.get_backend('aer_simulator'),
+                                   transpile_circ:bool=True
+                                   ):
+    """
+
+    :param n_qubits:
+    :param base_circuit:
+    :param pauli_string:
+    :param circ_name:
+    :param shadow_register:
+    :param device_backend:
+    :param transpile_circ:
+    :return:
+    """
+    # TODO: parallel construction of Pauli shadow circuits
+    pass
+
+def pauliShadow(
+        n_shadows:int,
+        n_qubits:int,
+        base_circuit:QuantumCircuit,
+        shadow_register:Union[QuantumRegister, List[QuantumRegister], List[int]],
+        device_backend=Aer.get_backend('aer_simulator'),
+        reps:int=1,
+        seed = 1701,
+        parallel:bool=True,
+        simulation:bool=True,
+        transpile:bool=False
+):
+    """
+    Shadow state tomography with random Pauli measurements
+    :param n_shadows: number of classical shadows
+    :param n_qubits: number of qubits involved for the classical shadows
+    :param base_circuit: the base circuit producing the target state
+    :param shadow_register: the quantum register involving the target state
+    :param device_backend: device to run/simulate the circuit
+    :param reps: number of repetitions/shots for the measurement
+    :param seed: random seed
+    :param parallel: whether to run in parallel
+    :param simulation: whether to run on a simulator
+    :param transpile: whether to transpile the circuit
+    :return:
+    """
+    if simulation and parallel:
+        N_WORKERS = 11
+        MAX_JOB_SIZE = 10
+        EXC = ThreadPoolExecutor(max_workers=N_WORKERS)
+        device_backend.set_options(executor=EXC)
+        device_backend.set_options(max_job_size=MAX_JOB_SIZE)
+        device_backend.set_options(max_parallel_experiments=0)
+    rng = np.random.default_rng(seed)
+    scheme = [rng.choice(['X', 'Y', 'Z'], size=n_qubits).tolist() for _ in range(n_shadows)]
+    shadow_circs = []
+    shadow_circ_name_pauli_dict = {}
+    for i in range(n_shadows):
+        print(i)
+        name = f"Shadow_{i}"
+        shadow_meas = ClassicalRegister(n_qubits, name="shadow")
+        qc_m = base_circuit.copy()
+        bit_string = scheme[i]
+        qc_bitstring = QuantumCircuit(n_qubits)
+        for j, bit in enumerate(bit_string):
+            bitGateMap(qc_bitstring, bit, j)
+        qc_m.append(qc_bitstring.to_instruction(), shadow_register)
+        qc_m.add_register(shadow_meas)
+        qc_m.measure(shadow_register, shadow_meas)
+        qc_m = qc_m.decompose(reps=4)
+        #TODO: parallel transpilation of the circuits
+        qc_m.name = name
+        shadow_circs.append(qc_m)
+        shadow_circ_name_pauli_dict[name] = bit_string
+    job = device_backend.run(shadow_circs, shots=reps)
+    result_dict = job.result().to_dict()["results"]
+    result_counts = job.result().get_counts()
+    shadows = []
+    for i in range(n_shadows):
+        name = result_dict[i]["header"]["name"]
+        counts = result_counts[i]
+        pauli_string = shadow_circ_name_pauli_dict[name]
+        for bit, count in counts.items():
+            mat = 1.
+            # reverse ordering since Qiskit is little-endian
+            for j, bi in enumerate(bit[::-1]):
+                b = rotGate(pauli_string[j])[int(bi), :]
+                mat = np.kron(Minv(1, np.outer(b.conj(), b)), mat)
+            shadows.append(mat * count)
     rho_shadow = np.sum(shadows, axis=0) / (n_shadows * reps)
     return rho_shadow
 
@@ -205,11 +312,12 @@ if __name__ == '__main__':
     omega = np.random.randn(1)
     eta = np.random.randn(12)
 
-    nShadows = 500
+    nShadows = 1000
 
     backbone = backboneCircFourQubitFeature(patches,theta, phi, gamma, omega, eta)
-    rho_actual = qiskit.quantum_info.partial_trace(DensityMatrix(backbone), [4,5,6,7,8,9]).data
-    rho_shadow = cliffordShadow(nShadows, 4, backbone, [0,1,2,3], transpile=True, parallel=True, simulation=True)
+    rho_shadow = pauliShadow(nShadows, 4, backbone, [0,1,2,3], transpile=True, parallel=True, simulation=True)
+    #rho_shadow = cliffordShadow(nShadows, 4, backbone, [0,1,2,3], transpile=True, parallel=True, simulation=True)
+    rho_actual = qiskit.quantum_info.partial_trace(DensityMatrix(backbone), [4, 5, 6, 7, 8, 9]).data
     print(complexMatrixDiff(rho_actual, rho_shadow))
     print(qiskit.quantum_info.state_fidelity(DensityMatrix(rho_shadow), DensityMatrix(rho_actual), validate= False))
 
@@ -218,20 +326,19 @@ if __name__ == '__main__':
     plt.imshow(rho_actual.real, vmax=0.7, vmin=-0.7)
     plt.subplot(122)
     plt.imshow(rho_actual.imag, vmax=0.7, vmin=-0.7)
-    plt.savefig("correct-clifford.png")
+    plt.savefig("correct-pauli.png")
 
     plt.subplot(121)
-    plt.suptitle(f"Shadow(Clifford)-{nShadows}-shadows")
+    plt.suptitle(f"Shadow(pauli)-{nShadows}-shadows")
     plt.imshow(rho_shadow.real, vmax=0.7, vmin=-0.7)
     plt.subplot(122)
     plt.imshow(rho_shadow.imag, vmax=0.7, vmin=-0.7)
-    plt.savefig(f"shadow-clifford-{nShadows}-shadows.png")
+    plt.savefig(f"shadow-pauli-{nShadows}-shadows.png")
 
     plot_state_city(rho_actual, title="Correct").savefig("correct-city.png")
-    plot_state_city(rho_shadow, title="Shadow (clifford)").savefig(
-        f"shadow-clifford-{nShadows}-shadows-city.png")
+    plot_state_city(rho_shadow, title="Shadow (pauli)").savefig(
+        f"shadow-pauli-{nShadows}-shadows-city.png")
     plt.close('all')
-
 
 
 
