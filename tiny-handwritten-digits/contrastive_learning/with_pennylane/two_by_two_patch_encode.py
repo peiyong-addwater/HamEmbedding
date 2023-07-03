adfc72e7943e026cc34b4754fec397feb6b271e9
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-07-03 18:31:36 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_pennylane/two_by_two_patch_encode.py
+++ tiny-handwritten-digits/contrastive_learning/with_pennylane/two_by_two_patch_encode.py
@@ -5,12 +5,14 @@ import numpy as np
 from typing import List, Tuple, Union
 from pennylane.wires import Wires
 import sys
+
 sys.path.insert(0, '/home/peiyongw/Desktop/Research/QML-ImageClassification')
 
-def four_pixel_two_qubits(
-        pixels:Union[list, np.ndarray, pnp.array],
-        parameters:Union[List, np.ndarray, pnp.array],
-        wires:Union[List[int], Wires]
+
+def FourPixelEncodeTwoQubits(
+        pixels: Union[list, np.ndarray, pnp.array],
+        parameters: Union[List, np.ndarray, pnp.array],
+        wires: Union[List[int], Wires]
 ):
     """
     :param pixels: 4-element array
@@ -24,9 +26,10 @@ def four_pixel_two_qubits(
         qml.RX(pixels[2], wires=wires[1])
         qml.RY(pixels[3], wires=wires[1])
         qml.CZ(wires=[wires[0], wires[1]])
-        qml.U3(parameters[6*i], parameters[6*i+1], parameters[6*i+2], wires=wires[0])
-        qml.U3(parameters[6*i+3], parameters[6*i+4], parameters[6*i+5], wires=wires[1])
+        qml.U3(parameters[6 * i], parameters[6 * i + 1], parameters[6 * i + 2], wires=wires[0])
+        qml.U3(parameters[6 * i + 3], parameters[6 * i + 4], parameters[6 * i + 5], wires=wires[1])
         qml.CZ(wires=[wires[0], wires[1]])
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -37,10 +40,13 @@ if __name__ == "__main__":
     theta = np.random.randn(12)
     dev = qml.device("default.qubit", wires=2)
     wires = Wires([0, 1])
+
+
     @qml.qnode(dev)
     def circuit(x, theta):
-        four_pixel_two_qubits(x, theta, wires=wires)
+        FourPixelEncodeTwoQubits(x, theta, wires=wires)
         return qml.state()
+
 
     fig, ax = qml.draw_mpl(circuit)(x, theta)
     plt.savefig("four_pixel_two_qubits_encode.png")
