88c19c0cccd9f05c5a2d3eacabb855b85de193d2
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-07-12 00:35:20 +1000

    updates

--- data/mini-digits/augment_images.py
+++ data/mini-digits/augment_images.py
@@ -1,7 +1,4 @@
 import Augmentor
+import os
 
-p = Augmentor.Pipeline("images")
-
-p.rotate(probability=0.9, max_left_rotation=10, max_right_rotation=10)
-p.random_distortion(probability=0.9, grid_width=4, grid_height=4, magnitude=8)
-p.sample(100)
+data_folder = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits/images"
