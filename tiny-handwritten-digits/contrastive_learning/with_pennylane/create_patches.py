9c53fb7d896f2f0734307a1828b16302b20eed9a
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-07-11 21:20:49 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_pennylane/create_patches.py
+++ tiny-handwritten-digits/contrastive_learning/with_pennylane/create_patches.py
@@ -19,6 +19,9 @@ def cut_8x8_to_2x2(img:np.ndarray):
     return patches
 
 if __name__ == '__main__':
+    from PIL import Image
+
+    image_png_dir = os.path.join(data_dir, "images")
 
     img = np.arange(64).reshape(8,8)
     print(img)
@@ -29,6 +32,25 @@ if __name__ == '__main__':
     with open(os.path.join(data_dir, "tiny-handwritten-as-rotation-angles.pkl"), 'rb') as f:
         mnist = pickle.load(f)
         train_imgs, train_labels, test_imgs, test_labels = mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
+
+    # save the individual images for inspection
+    counter = 0
+    for i in range(len(train_imgs)):
+        label = train_labels[i]
+        img_array = train_imgs[i]/(2*np.pi)*255
+        img = Image.fromarray(img_array.astype('uint8'))
+        save_name = f"image_train_{counter:04}_label_{label}.png"
+        img.save(os.path.join(image_png_dir, save_name))
+        counter += 1
+
+    for i in range(len(test_imgs)):
+        label = test_labels[i]
+        img_array = test_imgs[i]/(2*np.pi)*255
+        img = Image.fromarray(img_array.astype('uint8'))
+        save_name = f"image_test_{counter:04}_label_{label}.png"
+        img.save(os.path.join(image_png_dir, save_name))
+        counter += 1
+
 
     patched_img_data = dict()
 
