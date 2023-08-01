a6630dd6cfe4f385bf57c59e0f6b21f6db13d9ff
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-02 00:53:32 +1000

    updates

---
+++ tiny-handwritten-digits/contrastive_learning/with_pennylane_torch/two_by_two_patch_d_and_r.py
@@ -0,0 +1,13 @@
+import pennylane as qml
+from pennylane import numpy as pnp
+import torch
+import numpy as np
+from typing import List, Tuple, Union
+from utils import Reset0
+from pennylane.wires import Wires
+import sys
+sys.path.insert(0, '/home/peiyongw/Desktop/Research/QML-ImageClassification')
+
+from two_by_two_patch_encode import FourPixelEncodeTwoQubits
+from reset_gate import Reset0
+
