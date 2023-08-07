f02a1b13910d8171ca6616fef0f8f83c99ef090e
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-08 08:59:52 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_pennylane_torch_v2/four_by_four_patch_encode.py
+++ tiny-handwritten-digits/contrastive_learning/with_pennylane_torch_v2/four_by_four_patch_encode.py
@@ -10,18 +10,18 @@ sys.path.insert(0, '/home/peiyongw/Desktop/Research/QML-ImageClassification')
 
 def FourByFourPatchDataCirc(
         data: torch.Tensor,
-        loc: torch.Tensor,
         wires: Union[Wires, List[int]],
 ):
-    #TODO: change to 18-dim data input while the last two elements are the position encoding.
     """
     This function uses a modified SU(4) gate to encode 16 pixels into 3 qubits
     Since we are dealing with 8 by 8 images and we want to encode 4 by 4 patches,
-    the location tensor is a 2D tensor of shape (batchsize, 2),
-    the location
-    :param data: (batchsize, 16) tensor, containing the pixel values
-    :param loc:  (batchsize, 2) tensor, containing the location of the patch.
+    plus the positional encoding, which is a two-element vector with elements in {0,1},
+    for example, the patch on the upper left corner will have positional encoding (0,0),
+    which will be transformed into (0*pi/2, 0*pi/2) = (0,0) when acting as rotation angles of the gates.
+    then the input data dimension is (batchsize, 4x4+2) = (batchsize, 18)
+    :param data: (batchsize, 18) tensor, containing the pixel values in the first 16 elements, the last two elements contain the positional encoding.
     :param wires:
     :return:
     """
     assert len(wires) == 3
+    assert data.shape[1] == 18
