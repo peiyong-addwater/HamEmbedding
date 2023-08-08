a4c5422fa223666845f5ad3c5c69047d5e0c5f7d
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-09 00:38:37 +1000

    updates

--- data/mini-digits/create_annotations_file.py
+++ data/mini-digits/create_annotations_file.py
@@ -5,5 +5,10 @@ import pickle
 from PIL import Image
 import pandas as pd
 import os
+import csv
 
 img_dir = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits/images"
+save_dir = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits"
+
+image_filename = os.listdir(img_dir)
+print(image_filename)
