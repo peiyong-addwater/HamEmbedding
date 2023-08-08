808a907c5f5a6c6cc28e5a7df168dc2807bb3f38
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-09 00:34:35 +1000

    updates

---
+++ data/mini-digits/create_annotations_file.py
@@ -0,0 +1,9 @@
+import numpy as np
+from urllib import request
+import gzip
+import pickle
+from PIL import Image
+import pandas as pd
+import os
+
+img_dir = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits/images"
