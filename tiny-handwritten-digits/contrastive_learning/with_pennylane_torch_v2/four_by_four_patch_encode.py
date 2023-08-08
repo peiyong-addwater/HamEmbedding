99d76d443f1face29b6f254afc7e583781bf6b6d
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-08 19:08:34 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/with_pennylane_torch_v2/four_by_four_patch_encode.py
+++ tiny-handwritten-digits/contrastive_learning/with_pennylane_torch_v2/four_by_four_patch_encode.py
@@ -8,20 +8,47 @@ import sys
 
 sys.path.insert(0, '/home/peiyongw/Desktop/Research/QML-ImageClassification')
 
-def FourByFourPatchDataCirc(
-        data: torch.Tensor,
-        wires: Union[Wires, List[int]],
+def FourPixelReUploading(
+        pixels: torch.Tensor,
+        encode_params: torch.Tensor,
+        L1: int,
+        wires: Union[Wires, List[int]]
 ):
     """
-    This function uses a modified SU(4) gate to encode 16 pixels into 3 qubits
-    Since we are dealing with 8 by 8 images and we want to encode 4 by 4 patches,
-    plus the positional encoding, which is a two-element vector with elements in {0,1},
-    for example, the patch on the upper left corner will have positional encoding (0,0),
-    which will be transformed into (0*pi/2, 0*pi/2) = (0,0) when acting as rotation angles of the gates.
-    then the input data dimension is (batchsize, 4x4+2) = (batchsize, 18)
-    :param data: (batchsize, 18) tensor, containing the pixel values in the first 16 elements, the last two elements contain the positional encoding.
-    :param wires:
+    Data re-uploading layer for four pixels. Pixel data is encoded as rotation parameters of RX and RY gates;
+    Entanglement is achieved by CZ gates.
+    Trainable parameters are the rotation parameters of Rot gates
+    :param pixels: input pixel data. Four element array
+    :param encode_params: trainable parameters for encoding. 6*L1 element array
+    :param L1: number of data re-uploading layers
+    :param wires: Qubits to be used. Number of qubits: 2
     :return:
     """
-    assert len(wires) == 3
-    assert data.shape[1] == 18
+    assert len(wires) == 2 # Number of qubits must be 2
+    for i in range(L1):
+        qml.RX(pixels[0], wires=wires[0])
+        qml.RY(pixels[1], wires=wires[0])
+        qml.RX(pixels[2], wires=wires[1])
+        qml.RY(pixels[3], wires=wires[1])
+        qml.CZ(wires=[wires[0], wires[1]])
+        qml.Rot(encode_params[6*i], encode_params[6*i+1], encode_params[6*i+2], wires=wires[0])
+        qml.Rot(encode_params[6*i+3], encode_params[6*i+4], encode_params[6*i+5], wires=wires[1])
+        qml.CZ(wires=[wires[0], wires[1]])
+        qml.Barrier()
+
+
+if __name__ == '__main__':
+    dev2q = qml.device('default.qubit', wires=2)
+    dev4q = qml.device('default.qubit', wires=4)
+    @qml.qnode(dev2q, interface='torch')
+    def four_pixel_encode(pixels, encode_params, L1):
+        FourPixelReUploading(pixels, encode_params, L1, wires=[0, 1])
+        return qml.probs()
+
+    pixels = torch.randn(4,5)
+    L1 = 2
+    encode_params = torch.randn(L1*6)
+
+    print(four_pixel_encode(pixels, encode_params, L1))
+    fig, ax = qml.draw_mpl(four_pixel_encode, style='sketch')(pixels[:,0], encode_params, L1)
+    fig.savefig('four_pixel_reupload.png')
