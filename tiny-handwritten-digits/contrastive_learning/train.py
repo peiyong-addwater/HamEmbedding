38a74dfcc24f70887989d802f16f74e10a4ed11f
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-15 23:25:34 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/train.py
+++ tiny-handwritten-digits/contrastive_learning/train.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     img_dir = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits/images"
     csv_file = "/home/peiyongw/Desktop/Research/QML-ImageClassification/data/mini-digits/annotated_labels.csv"
 
-    BATCH_SIZE = 500
+    BATCH_SIZE = 4
     EPOCHS = 100
 
     # structural parameters
