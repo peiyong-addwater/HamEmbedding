f400b449ee9893aa0eca6e86ac78b98ca0f52df9
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-16 16:17:58 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/try_stuff.py
+++ tiny-handwritten-digits/contrastive_learning/try_stuff.py
@@ -24,6 +24,13 @@ if __name__ == '__main__':
     patch_encode_params = torch.randn(L2*L1 * 6 * 4)
     patch_rot_crot_params = torch.randn(L2* 21)
 
+    weight_shapes = {
+        "four_pixel_encode_params": L2 * L1 * 6 * 4,
+        "sixteen_pixel_encode_params": L2 * 21,
+        "mem_init_params": 12 * mem_qubits - 9,
+        "mem_patch_interact_params": 36,  # currently only support 2-to-2 interaction
+        "mem_computation_params": L_MC*(12 * mem_qubits - 9)}
+
     #sizes
     print(L2*L1 * 6 * 4)
     print(L2* 21)
@@ -56,6 +63,8 @@ if __name__ == '__main__':
         )
         return qml.probs(wires[:mem_qubits])
 
-    print(qnn_probs(data, patch_encode_params, patch_rot_crot_params, mem_init_params, mem_patch_interact_params, mem_computation_params))
+    #print(qnn_probs(data, patch_encode_params, patch_rot_crot_params, mem_init_params, mem_patch_interact_params, mem_computation_params))
+    qlayer = TorchLayer(qnn_probs, weight_shapes=weight_shapes)
+    print(qlayer.qnode_weights)
 
 
