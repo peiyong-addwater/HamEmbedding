f23d6086a136083cd7e7f369dac8b02adad802f4
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-08-29 12:18:18 +1000

    updates

--- tiny-handwritten-digits/contrastive_learning/jobScript.sh
+++ tiny-handwritten-digits/contrastive_learning/jobScript.sh
@@ -12,4 +12,4 @@ module --ignore-cache load python/3.10.8 py-pip/22.2.2-py3.10.8 py-setuptools/57
 export PYTHONUSERBASE=/software/projects/pawsey0419/peiyongw/setonix/python
 export PATH=$PATH:$PYTHONUSERBASE/bin
 
-python3 train_pawsey_cpu.py --img_dir /scratch/pawsey0419/peiyongw/QML-ImageClassification/data/mini-digits/images --csv_file /scratch/pawsey0419/peiyongw/QML-ImageClassification/data/mini-digits/annotated_labels.csv --batch_size 50 --train_batches 20 --epoches 50 --n_mem_qubits 4 --n_patch_qubits 4 --L1 2 --L2 2 --L_MC 1 --reset_first_mem_qubit False --working_dir /scratch/pawsey0419/peiyongw/QML-ImageClassification/tiny-handwritten-digits/contrastive_learning
+python3 train_pawsey_cpu.py --img_dir /scratch/pawsey0419/peiyongw/QML-ImageClassification/data/mini-digits/images --csv_file /scratch/pawsey0419/peiyongw/QML-ImageClassification/data/mini-digits/annotated_labels.csv --batch_size 50 --train_batches 20 --epochs 50 --n_mem_qubits 4 --n_patch_qubits 4 --L1 2 --L2 2 --L_MC 1 --reset_first_mem_qubit False --working_dir /scratch/pawsey0419/peiyongw/QML-ImageClassification/tiny-handwritten-digits/contrastive_learning
