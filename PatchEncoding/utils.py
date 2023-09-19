4bc7ba705b19d8e3d1ee74d9aa15116875f4c686
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-09-20 04:10:30 +1000

    updates

---
+++ PatchEncoding/utils.py
@@ -0,0 +1,9 @@
+import torch
+
+def cut8x8to4x4PatchesNoPos(img:torch.Tensor):
+    batchsize = img.shape[0]
+    patches = torch.zeros((batchsize, 64))
+    for i in range(2):
+        for j in range(2):
+            patches[:, 16*(2*i+j):16*(2*i+j+1)] = img[:, 4*i:4*i+4, 4*j:4*j+4].flatten(start_dim=1)
+    return patches
