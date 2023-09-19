13a88c1f65530d7f00fd912ca89acb3af4274f0a
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-09-20 05:57:21 +1000

    updates

--- Models/PatchEncoding/utils.py
+++ Models/PatchEncoding/utils.py
@@ -1,6 +1,6 @@
 import torch
 
-def cut8x8GreyScaleto4x4PatchesNoPos(img:torch.Tensor):
+def cut8x8GreyScaleYo4x4PatchesNoPos(img:torch.Tensor):
     """
     Cut 8x8 image to 4x4 patches without position encoding
     Applied to grey scale/binary-valued image
@@ -13,3 +13,16 @@ def cut8x8GreyScaleto4x4PatchesNoPos(img:torch.Tensor):
         for j in range(2):
             patches[:, 16*(2*i+j):16*(2*i+j+1)] = img[:, 4*i:4*i+4, 4*j:4*j+4].flatten(start_dim=1)
     return patches
+
+def cut32x32GreyScaleTo8x8PatchesNoPos(img:torch.Tensor):
+    """
+    Cut 32x32 image to 8x8 patches without position encoding
+    Applied to grey scale/binary-valued image
+    :param img: shape (batchsize, 32, 32)
+    :return: patches: shape (batchsize, 1024)
+    """
+    batchsize = img.shape[0]
+    patches = torch.zeros((batchsize, 1024))
+    for i in range(4):
+        for j in range(4):
+            patches[:, 64*(4*i+j):64*(4*i+j+1)] = img[:, 8*i:8*i+8, 8*j:8*j+8].flatten(start_dim=1)
