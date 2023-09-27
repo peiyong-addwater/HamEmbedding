1616f2e87096cf70142fa9a3e0e83a93f438df87
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-09-27 18:39:28 +1000

    updates

--- Models/qiskit_models.py
+++ Models/qiskit_models.py
@@ -1,1 +1,44 @@
 # models based on Qiskit and PyTorch
+import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.circuit import Parameter, ParameterVector
+from qiskit.circuit.parametervector import ParameterVectorElement
+from qiskit.circuit import Qubit
+from typing import Any, Callable, Optional, Sequence, Tuple, List, Union
+from math import pi
+from Layers.qk.qiskit_layers import createMemStateInitCirc, createMemCompCirc, createMemPatchInteract
+from PatchEncoding.qk.PatchEmbedding import fourByFourPatchReupload, create8x8ReUploading
+from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
+from qiskit_machine_learning.connectors import TorchConnector
+from qiskit.algorithms.gradients import SPSASamplerGradient
+from qiskit.primitives import BaseSampler, SamplerResult, Sampler
+from qiskit.utils import algorithm_globals
+from qiskit.quantum_info import SparsePauliOp
+
+QiskitParameter = Union[ParameterVector, List[Parameter], List[ParameterVectorElement]]
+QiskitQubits = Union[List[int], List[Qubit], QuantumRegister]
+
+def createBackbone8x8Image(
+        pixels: QiskitParameter,
+        params: QiskitParameter,
+        num_single_patch_reuploading: int=2,
+        num_mem_qubits:int = 2,
+        num_mem_interact_qubits:int = 2,
+        num_patch_interact_qubits:int = 2,
+        num_mem_comp_layers:int=1
+)->QuantumCircuit:
+    """
+    Creates a (num_mem_qubits+3)-qubit circuit that encodes an 8x8 image into num_mem_qubits qubits,
+    with trainable parameters for data re-uploading, and trainable parameters for the memory-related computations
+    Args:
+        pixels:
+        params:
+        num_single_patch_reuploading:
+        num_mem_qubits:
+        num_mem_interact_qubits:
+        num_patch_interact_qubits:
+        num_mem_comp_layers:
+
+    Returns:
+
+    """
