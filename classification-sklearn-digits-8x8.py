225a597c72d24dc6dd954e9e18e1f4f3f7624bd6
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-10-04 09:07:58 +1100

    updates

--- classification-sklearn-digits-8x8.py
+++ classification-sklearn-digits-8x8.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     parser.add_argument('--spsa_batchsize', type=int, required=False, default=1)
     parser.add_argument('--working_dir', type=str, required=False, default='/home/peiyongw/Desktop/Research/QML-ImageTask')
     parser.add_argument('--prev_checkpoint', type=str, required=False, default=None)
-    parser.add_argument('--n_single_patch_reupload', type=int, required=False, default=2)
+    parser.add_argument('--n_single_patch_reupload', type=int, required=False, default=1)
 
     args = parser.parse_args()
     wd = args.working_dir
