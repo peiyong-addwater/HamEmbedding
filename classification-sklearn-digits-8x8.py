1239a5bd64923dea28381f815622d5fd931043c3
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-10-04 08:13:12 +1100

    updates

--- classification-sklearn-digits-8x8.py
+++ classification-sklearn-digits-8x8.py
@@ -32,8 +32,8 @@ if __name__ == '__main__':
     import torchmetrics
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', type=int, required=False, default=100)
-    parser.add_argument('--train_batches', type=int, required=False, default=10)
+    parser.add_argument('--batch_size', type=int, required=False, default=200)
+    parser.add_argument('--train_batches', type=int, required=False, default=5)
     parser.add_argument('--epochs', type=int, required=False, default=500)
     parser.add_argument('--n_mem_qubits', type=int, required=False, default=2)
     parser.add_argument('--n_mem_interact_qubits', type=int, required=False, default=1)
